26a8803c01cdbe096059c203aebff375186d6560
Kanem <kanemdeng@email.com>
2022-07-13 11:17:46 -0700

    added gem valid_email from https://rubygems.org/ to check if the eamill address is validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+
+# rails g model password_digest
+#   then in class has_secure_password
+# this will run bcrypt
+# will add passwor_confirmation
+# make sure bcrypt is installed
+
+# require 'valid_email'
+
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, email: true
 end
