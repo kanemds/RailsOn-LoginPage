ba30c4801d1871e7caf049a60568a4145d0e4766
Kanem <kanemdeng@email.com>
2022-07-15 10:58:48 -0700

    added: forgot password will received an e-mail to reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,7 +4,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(params[:eamil])
+    @user = User.find_by(email: params[:email])
 
     if @user.present?
       # found by App/mailers, rails g mailer Passwor reset
@@ -13,5 +13,29 @@ class PasswordResetsController < ApplicationController
 
       redirect_to root_path, notice: "E-mail has been sent, please check your mail box to reset your password"
   end
+
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "The password reset request is expired, please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+    if @user.update(password_params)
+
+      redirect_to sign_in_path, notice: "Password reset successfully!"
+    else
+      render :edit
+    end
+  end
+
+  private
+    
+    def password_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
 end 
